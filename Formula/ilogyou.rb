d6277c0824ef1ba4c5b17edf493ca6b8178dcfdc
Travis CI <travis@Traviss-Mac-6.local>
2019-02-22 15:50:47 +0000

    Update Formula

--- Formula/ilogyou.rb
+++ Formula/ilogyou.rb
@@ -1,9 +1,9 @@
 class Ilogyou < Formula
   desc "Swift Framework for membership handling"
   homepage "https://github.com/leolgrn/ilogyou/"
-  url "https://github.com/leolgrn/ilogyou/releases/download/145200-22022019/ilogyou.tar.gz"
+  url "https://github.com/leolgrn/ilogyou/releases/download/155046-22022019/ilogyou.tar.gz"
   version "1.0.0"
-  sha256 "96b8197d5d85f0e173954965c862eac7bccedd47772fce866387a6d780521c5a"
+  sha256 "3917ca17024b056cd0b748e843772df6f43f55e40eebc3e3f290b80b183d7749"
 
   def install
     bin.install "ilogyou"
